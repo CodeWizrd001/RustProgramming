4a4ddb5ba39f5b8ce1851295d708b65e0ba6635c
CodeWizard <codewizard001@gmail.com>
2020-12-05 18:22:00 +0530

    Initial Commit

---
+++ alpha/target/debug/alpha.d
@@ -0,0 +1,1 @@
+/home/wizard/Work/RustProgramming/alpha/target/debug/alpha: /home/wizard/Work/RustProgramming/alpha/src/main.rs
---
+++ alpha/target/rls/debug/deps/alpha-680e02c4c3118d6b.d
@@ -0,0 +1,5 @@
+/home/wizard/Work/RustProgramming/alpha/target/rls/debug/deps/alpha-680e02c4c3118d6b.rmeta: src/main.rs
+
+/home/wizard/Work/RustProgramming/alpha/target/rls/debug/deps/alpha-680e02c4c3118d6b.d: src/main.rs
+
+src/main.rs:
